9851c213b0490de7184d00febf6b04fe84cf4efd
jwhowes <howesjacob8@gmail.com>
2025-03-16 11:53:10 +0000

    Now saves every 100 iterations

--- src/trainer.py
+++ src/trainer.py
@@ -6,6 +6,7 @@ from dataclasses import dataclass
 import torch
 import torch.nn.functional as F
 from accelerate import Accelerator
+from transformers import get_cosine_schedule_with_warmup
 
 from .config import BaseConfig
 from .model.transformer import TransformerConfig, Transformer
@@ -17,7 +18,6 @@ accelerator = Accelerator()
 
 @dataclass
 class TrainerConfig(BaseConfig):
-    num_epochs: int = 5
     log_interval: int = 100
 
     lr: float = 5e-5
@@ -40,7 +40,7 @@ class Trainer:
 
             self.log_path = os.path.join(self.exp_dir, "log.csv")
             with open(self.log_path, "w+") as f:
-                f.write("epoch,average loss,timestamp\n")
+                f.write("ckpt,average loss,timestamp\n")
 
             self.model_config = TransformerConfig.from_yaml(os.path.join(config_dir, "model.yaml"))
             with open(os.path.join(self.exp_dir, "model.yaml"), "w+") as f:
@@ -54,7 +54,7 @@ class Trainer:
             with open(os.path.join(self.exp_dir, "train.yaml"), "w+") as f:
                 yaml.dump(self.train_config, f)
 
-        self.epoch = 1
+        self.ckpt = 1
 
     def log(self, model: Transformer, average_loss: float):
         accelerator.wait_for_everyone()
@@ -62,13 +62,13 @@ class Trainer:
         if accelerator.is_main_process:
             torch.save(
                 accelerator.get_state_dict(model),
-                os.path.join(self.checkpoints_dir, f"checkpoint_{self.epoch:03}.pt")
+                os.path.join(self.checkpoints_dir, f"checkpoint_{self.ckpt:03}.pt")
             )
 
             with open(self.log_path, "a") as f:
-                f.write(f"{self.epoch},{average_loss},{datetime.now()}")
+                f.write(f"{self.ckpt},{average_loss},{datetime.now()}")
 
-        self.epoch += 1
+        self.ckpt += 1
 
     def train(self):
         model = Transformer.from_config(self.model_config)
@@ -78,36 +78,41 @@ class Trainer:
         opt = torch.optim.AdamW(
             model.parameters(), lr=self.train_config.lr, weight_decay=self.train_config.weight_decay
         )
+        lr_scheduler = get_cosine_schedule_with_warmup(
+            opt,
+            num_warmup_steps=0,
+            num_training_steps=len(dataloader)
+        )
 
-        model, dataloader, opt = accelerator.prepare(
-            model, dataloader, opt
+        model, dataloader, opt, lr_scheduler = accelerator.prepare(
+            model, dataloader, opt, lr_scheduler
         )
 
         total_loss = 0
-        for epoch in range(self.train_config.num_epochs):
-            if accelerator.is_main_process:
-                print(f"EPOCH {epoch + 1} / {self.train_config.num_epochs}")
+        prev_ckpt = 0
+        model.train()
+        for i, encoding in enumerate(dataloader):
+            opt.zero_grad()
 
-            model.train()
-            for i, encoding in enumerate(dataloader):
-                opt.zero_grad()
+            pred = model(encoding["prefix"])
+            loss = F.cross_entropy(pred.transpose(1, 2), encoding["suffix"], reduction="none")
+            loss[encoding["pad_mask"]] = 0
+            loss = loss.sum() / (~encoding["pad_mask"]).sum()
 
-                pred = model(encoding["prefix"])
-                loss = F.cross_entropy(pred.transpose(1, 2), encoding["suffix"], reduction="none")
-                loss[encoding["pad_mask"]] = 0
-                loss = loss.sum() / (~encoding["pad_mask"]).sum()
+            accelerator.backward(loss)
+            accelerator.clip_grad_norm_(model.parameters(), self.train_config.clip_grad)
 
-                accelerator.backward(loss)
-                accelerator.clip_grad_norm_(model.parameters(), self.train_config.clip_grad)
+            opt.step()
+            lr_scheduler.step()
 
-                opt.step()
+            total_loss += loss.item()
 
-                total_loss += loss.item()
-                if accelerator.is_main_process and i % self.train_config.log_interval == 0:
-                    print(f"\t{i} / {len(dataloader)} iters.\tLoss: {loss.item():.4f}")
+            if i % self.train_config.log_interval == 0:
+                average_loss = total_loss / (i - prev_ckpt + 1)
 
-            average_loss = total_loss / len(dataloader)
-            if accelerator.is_main_process:
-                print(f"Average Loss: {average_loss:.4f}")
+                if accelerator.is_main_process:
+                    print(f"\t{i} / {len(dataloader)} iters.\tAverage loss: {average_loss:.4f}")
 
-            self.log(model, average_loss)
+                self.log(model, average_loss)
+
+                prev_ckpt = i + 1
